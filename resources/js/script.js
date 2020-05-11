7f643f4d36873d7a61099ba4297c1e3132c7892f
Genat Hadri <gh43836@ubt-uni.net>
2020-05-11 21:45:00 +0200

    js ne folder

---
+++ resources/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    
+    
+    /* For the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+    
+    
+    /* Scroll on buttons */
+    $('.js--scroll-to-meals').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-meals').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    });
+    
+    
+    /* Navigation scroll */
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+    
+    
+});
