4d2485ddfbdd70d15b7b674fdedaa5132dc7d7de
BlinSahatqija <blinsahatqija@gmail.com>
2020-05-12 22:29:13 +0200

    vazhdim i pjeses paraprake

--- resources/js/script.js
+++ resources/js/script.js
@@ -39,6 +39,46 @@ $(document).ready(function() {
       });
     });
     
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
     
+//    $('.js--wp-2').waypoint(function(direction) {
+//        $('.js--wp-2').addClass('animated fadeInUp');
+//    }, {
+//        offset: '50%'
+//    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+//    $('.js--wp-4').waypoint(function(direction) {
+//        $('.js--wp-4').addClass('animated pulse');
+//    }, {
+//        offset: '50%'
+//    });
+//    
+    
+    /* Mobile navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }        
+    });
     
 });
