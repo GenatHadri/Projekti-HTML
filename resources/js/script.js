c868f0a112661765838a2b63cf0547a8c7c027b2
Genat Hadri <gh43836@ubt-uni.net>
2020-05-14 19:46:37 +0200

    pjesa e log in dhe disa ndryshime te vogla

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
     
     
@@ -23,48 +24,7 @@ $(document).ready(function() {
     });
     
     
-    /* Navigation scroll */
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    });
-    
-    /* Animations on scroll */
-    $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-    
-//    $('.js--wp-2').waypoint(function(direction) {
-//        $('.js--wp-2').addClass('animated fadeInUp');
-//    }, {
-//        offset: '50%'
-//    });
-    
-    $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
-    }, {
-        offset: '50%'
-    });
-    
-//    $('.js--wp-4').waypoint(function(direction) {
-//        $('.js--wp-4').addClass('animated pulse');
-//    }, {
-//        offset: '50%'
-//    });
-//    
-    
+  
     /* Mobile navigation */
     $('.js--nav-icon').click(function() {
         var nav = $('.js--main-nav');
@@ -81,4 +41,7 @@ $(document).ready(function() {
         }        
     });
     
+    
+    
 });
+
